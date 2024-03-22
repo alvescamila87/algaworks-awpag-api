52e1c0dbadb972612f94b1577ea6a1c1a58bf102
Camila <alvescamila87@gmail.com>
2024-03-22 19:36:13 -0300

    jakarta persistence: change model, controller and application properties

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package com.algaworks.awpag.api.controller;
 
 import com.algaworks.awpag.domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,21 +12,12 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        var cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Princesa Isabel de Bragança");
-        cliente1.setTelefone("47 99999-1111");
-        cliente1.setEmail("princesa_isable@algaworks.com");
-
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Dom Manoel");
-        cliente2.setTelefone("47 88888-2222");
-        cliente2.setEmail("dom_manoel@algaworks.com");
-
-        return Arrays.asList(cliente1, cliente2);
+        return manager.createQuery("from Cliente", Cliente.class).getResultList();
     }
 
 
--- src/main/java/com/algaworks/awpag/domain/model/Cliente.java
+++ src/main/java/com/algaworks/awpag/domain/model/Cliente.java
@@ -1,15 +1,27 @@
 package com.algaworks.awpag.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+//@Table(name = "tb_cliente")
 @Getter
 @Setter
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // @Column(name = "nome")
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 }
