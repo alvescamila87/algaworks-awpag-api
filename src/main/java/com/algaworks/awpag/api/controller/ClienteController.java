9d63edb8f7caff49614c5b3b9d0db9b6b9f39b59
Camila <alvescamila87@gmail.com>
2024-03-23 11:42:44 -0300

    spring data jpa: create repository, change controller to receive getClients

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,23 +1,33 @@
 package com.algaworks.awpag.api.controller;
 
 import com.algaworks.awpag.domain.model.Cliente;
+import com.algaworks.awpag.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    //@PersistenceContext
+    //private EntityManager manager;
+
+    //@Autowired
+    private final ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        return manager.createQuery("from Cliente", Cliente.class).getResultList();
+        //return manager.createQuery("from Cliente", Cliente.class).getResultList();
+        return  clienteRepository.findAll();
+        //return clienteRepository.findByNome("Princesa Isabel");
+        //return clienteRepository.findByNomeContaining("Is");
     }
 
 
