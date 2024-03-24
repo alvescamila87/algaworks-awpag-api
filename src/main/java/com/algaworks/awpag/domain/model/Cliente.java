f1c1435ba4273af4800420935a9f222268a31baa
Camila <alvescamila87@gmail.com>
2024-03-24 17:30:27 -0300

    create validation with jakarta validation at the class and controller: post and put methods

--- src/main/java/com/algaworks/awpag/domain/model/Cliente.java
+++ src/main/java/com/algaworks/awpag/domain/model/Cliente.java
@@ -1,6 +1,9 @@
 package com.algaworks.awpag.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,9 +21,17 @@ public class Cliente {
     private Long id;
 
     // @Column(name = "nome")
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
 
+    @NotBlank
+    @Size(max = 20)
     @Column(name = "fone")
     private String telefone;
 
