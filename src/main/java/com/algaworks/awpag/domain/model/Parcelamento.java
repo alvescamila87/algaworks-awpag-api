7e97e396df530fcd7e904fd96a5e6885e01f7d09
Camila <alvescamila87@gmail.com>
2024-03-24 22:01:04 -0300

    create validation at the parcelamento class and change parcelamento controller post

--- src/main/java/com/algaworks/awpag/domain/model/Parcelamento.java
+++ src/main/java/com/algaworks/awpag/domain/model/Parcelamento.java
@@ -1,6 +1,7 @@
 package com.algaworks.awpag.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,12 +20,23 @@ public class Parcelamento {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotNull
     @ManyToOne
     //@JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
+    @NotBlank
+    @Size(max = 20)
     private String descricao;
+
+    @NotNull
+    @Positive
     private BigDecimal valorTotal;
+
+    @NotNull
+    @Positive
+    @Max(12)
     private Integer quantidadeParcelas;
+
     private LocalDateTime dataCriacao;
 }
